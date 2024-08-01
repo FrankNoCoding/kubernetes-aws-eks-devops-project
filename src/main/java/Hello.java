6e979a43ee80d9c8f50c41f6fcfd2e6ffc16c218
asan <asan@gmail.com>
2024-08-01 17:25:49 +0530

    change in java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -10,7 +10,7 @@ public class Hello extends HttpServlet {
 
    public void init() throws ServletException {
       // Do required initialization
-      message = "HI Hello Devops world!";
+      message = "Hi Hello Devops world!";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
